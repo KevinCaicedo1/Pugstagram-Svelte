43e5da42767023611537870bab24ae590fd96b84
kevincaicedo1 <kocaicedo@espe.edu.ec>
2021-10-04 23:54:35 -0500

    Pugstagram netlify

---
+++ src/components/Profile.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let nickname;
+    export let name;
+</script>
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://arepa.s3.amazonws.com/oscar.png" alt="">
+        </div>
+        <div class="Profile-info">
+            <h2>
+                {nickname}
+            </h2>
+            <span>
+                {name}
+            </span>
+        </div>
+    </div>
+</div>
